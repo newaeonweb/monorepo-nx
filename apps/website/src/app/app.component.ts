051e11e6887592cc4388bab6b74753b06fe249f8
newaeonweb <fsilvasmonteiro@gmail.com>
2022-08-14 14:56:48 -0300

    initial commit

---
+++ apps/website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'website';
+}
